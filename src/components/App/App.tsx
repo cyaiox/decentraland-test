2903de420357d74a3d9347564ba6f435ae631e8d
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-05-08 17:43:02 -0300

    Add react-router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,74 +1,48 @@
 import React from 'react';
-import {
-  Button,
-  Card,
-  Center,
-  Footer,
-  Header,
-  Navbar,
-  Page,
-} from 'decentraland-ui';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Button, Center, Footer, Navbar, Page } from 'decentraland-ui';
 import { Props } from './App.types';
 import './App.css';
-import { TransferModal } from '../TransferModal';
+import { Home } from '../Home';
+import { Transfer } from '../Transfer';
 
 const App: React.FC<Props> = ({
-  address,
-  balance,
   isConnected,
   onConnect,
   isConnecting,
-  isTransfered,
   error,
 }) => {
-  const [openTransferModal, setOpenTransferModal] =
-    React.useState<boolean>(false);
-
-  React.useEffect(() => {
-    if (isTransfered) setOpenTransferModal(false);
-  }, [isTransfered]);
-
   const TransferButton: React.FC<{ isConnected: boolean }> = ({
     isConnected,
   }) => {
-    return isConnected ? <Button primary>Transfer</Button> : <></>;
+    return isConnected ? (
+      <Link to="/transfer">
+        <Button primary>Transfer</Button>
+      </Link>
+    ) : (
+      <></>
+    );
   };
 
   return (
     <>
       <Navbar rightMenu={<TransferButton isConnected={isConnected} />} />
       <Page className="App">
-        <Center>
-          {!isConnected ? (
-            <>
-              <Button primary onClick={onConnect} loading={isConnecting}>
-                Connect
-              </Button>
-              {error ? <p className="error">{error}</p> : null}
-            </>
-          ) : (
-            <Card>
-              <Header>Wallet</Header>
-              <p>
-                <strong>Address:</strong>&nbsp;
-                {address.slice(0, 6) + '...' + address.slice(-4)}
-              </p>
-              <p>
-                <strong>Balance:</strong>&nbsp;
-                {balance}&nbsp;{'DUMMY'}&nbsp;
-                <Button basic onClick={() => setOpenTransferModal(true)}>
-                  Transfer
-                </Button>
-              </p>
-            </Card>
-          )}
-        </Center>
+        {!isConnected ? (
+          <Center>
+            <Button primary onClick={onConnect} loading={isConnecting}>
+              Connect
+            </Button>
+            {error ? <p className="error">{error}</p> : null}
+          </Center>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="transfer" element={<Transfer />} />
+          </Routes>
+        )}
       </Page>
       <Footer />
-      <TransferModal
-        open={openTransferModal}
-        close={() => setOpenTransferModal(false)}
-      />
     </>
   );
 };
