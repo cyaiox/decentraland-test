f218d924397c13c960e92d3849809e47a33f32bc
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-05-13 07:49:40 -0300

    Add docker support

--- buildDockerImageRopsten.sh
+++ buildDockerImageRopsten.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build --build-arg REACT_APP_TOKEN_ADDRESS=0xA8a16e22ec7cEF817512f2F7AC08913177BA7e08 . -t decentraland/webapp:ropsten
+docker build . \
+    --no-cache \
+    --pull \
+    --build-arg REACT_APP_TOKEN_ADDRESS=0xA8a16e22ec7cEF817512f2F7AC08913177BA7e08 \
+    -f docker/prod/Dockerfile \
+    -t decentraland/webapp:ropsten
